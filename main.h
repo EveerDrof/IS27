ec5cd2f81a32b868465050369d93c6f0c1f0304b
EveerDrof <48711676+EveerDrof@users.noreply.github.com>
2019-09-28 19:02:27 +0300

    Добавил свои функции

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#pragma warning(disable : 4996)
 using namespace std;
 
 struct pixel
@@ -41,6 +41,14 @@ void read_file();
 void write_file();
 void help();
 
+//Konstantinov
+void chb();
+void raskras(bool, int, enum Color, enum Color);
+void limitcolors(int);
+enum Color { RED, BLUE, GREEN, GRAY };
+Color Color_Check(char *CN);
+
+
 int get_pad(int width)
 {
 	int i=0;
@@ -64,6 +72,9 @@ void help()
     cout << "  chbmp resizeh N in_file out_file \n";
     cout << "  chbmp rotater  in_file out_file \n";
     cout << "  chbmp rotatel  in_file out_file \n";
+	cout << "  chbmp chb  in_file out_file \n";
+	cout << "  chbmp raskras  in_file out_file \n";
+	cout << "  chbmp limitcolors  in_file out_file \n";
 }
 
 int main(int argc, char** argv)
@@ -76,7 +87,8 @@ int main(int argc, char** argv)
 		return 1;
     }
   
-    if(strcmp(argv[1],"resizev") && strcmp(argv[1],"resizeh") && strcmp(argv[1],"rotater") &&  strcmp(argv[1],"rotatel"))
+    if(strcmp(argv[1],"resizev") && strcmp(argv[1],"resizeh") && strcmp(argv[1],"rotater") &&  strcmp(argv[1],"rotatel")
+		&& strcmp(argv[1], "chb") && strcmp(argv[1], "raskras") && strcmp(argv[1], "limitcolors"))
     {
 		help();
 		return 1;
@@ -131,6 +143,54 @@ int main(int argc, char** argv)
 		outfile=argv[3];
 		rotatel();
     }
+	if (!strcmp(argv[1], "chb"))
+	{
+		if (argc < 4)
+		{
+			help();
+			return 1;
+		}
+		infile = argv[2];
+		outfile = argv[3];
+		chb();
+
+	}
+	if (!strcmp(argv[1], "raskras"))
+	{
+		infile = argv[2];
+		outfile = argv[3];
+		Color COL;
+		Color COL2;
+		if (argc < 6)
+		{
+			help();
+			return 1;
+		}
+		COL = Color_Check(argv[5]);
+		if (argc == 6)
+		{
+			COL2 = COL;
+			raskras(true, atoi(argv[4]), COL, COL2);
+		}
+		else
+		{
+			COL2 = Color_Check(argv[6]);
+			raskras(false, atoi(argv[4]), COL, COL2);
+		}
+
+	}			
+	if (!strcmp(argv[1], "limitcolors"))
+		{
+			if (argc < 4)
+			{
+				help();
+				return 1;
+			}
+			infile = argv[2];
+			outfile = argv[3];
+			limitcolors(atoi(argv[4]));
+
+		}
     return 0;
 }
 
