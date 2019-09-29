1fd177cd28bd333ab952a0c4c64276c74ebe7bad
raibook <55733505+raibook@users.noreply.github.com>
2019-09-29 19:18:08 +0300

    Create vershinin.h
    Создал фаил vershinin.h со своими функциями

---
+++ vershinin.h
@@ -0,0 +1,22 @@
+#include "utilities.h"
+
+void cutFromPic(struct pointOfPixel, struct pointOfPixel);
+
+void cutFromPic(struct pointOfPixel point1, struct pointOfPixel point2)
+{
+	p_out = new struct pixel*[abs(point2.y - point1.y)];
+	for (int i = 0; i < abs(point2.y - point1.y); i++)
+	{
+		p_out[i] = new struct pixel[abs(point2.x - point1.x)];
+	}
+	for (int i = 0; i < abs(point2.y - point1.y); i++)
+	{
+		for (int j = 0; j < abs(point2.x - point1.x); j++)
+		{
+			p_out[i][j] = p_in[i + int(point1.y)][j + int(point1.x)];
+		}
+	}
+	h_bmp.height = abs(point2.y - point1.y);
+	h_bmp.width = abs(point2.x - point1.x);
+
+}
